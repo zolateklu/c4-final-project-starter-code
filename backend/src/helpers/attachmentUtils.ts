941478a994a48e28c5cf748e05b3ce6c13914bdf
zolateklu <zolateklu@gmail.com>
2022-12-14 12:14:10 +0300

    upload image fixed

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -5,7 +5,7 @@ const XAWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the fileStogare logic
 const s3BucketName = process.env.ATTACHMENT_S3_BUCKET
-const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+const urlExpiration = 300
 
 export class AttachmentUtils{
     constructor(
